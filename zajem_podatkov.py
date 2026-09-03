97d737087d56c3ffcc8f2cbf52cc14e4cffd0adc
Zala Kmetič <zalakmetic@gmail.com>
2026-09-03 20:07:11 +0200

    Branje shranjene HTML datoteke.

--- zajem_podatkov.py
+++ zajem_podatkov.py
@@ -1,4 +1,5 @@
 import requests
+import bs4
 
 link = "https://www.statbunker.com/competitions/GoalsFor?comp_id=790"
 
@@ -7,9 +8,16 @@ def prenesi_in_shrani_html(url, ime_datoteke):
 
     if response.status_code == 200:
         print("HTML je uspešno prenesen.")
-        with open(ime_datoteke, "w", encoding='utf-8') as f:
+        with open(ime_datoteke, "w", encoding="utf-8") as f:
             f.write(response.text)
     else:
         print("Prišlo je do napake.")
 
 prenesi_in_shrani_html(link, "podatki/goals_for.html")
+
+def preberi_html(ime_datoteke):
+    with open(ime_datoteke, "r", encoding="utf-8") as f:
+        return f.read()
+
+html = preberi_html("podatki/goals_for.html")
+
